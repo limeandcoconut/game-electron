b447ac98411c5d5e08a6b973ef756f91f29e2dcd
Jacob Smith <js@jacobsmith.tech>
2019-09-16 16:34:50 -0700

    Move to eslint-config-limeandcoconut

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
+'use strict'
+
 module.exports = {
-    extends: 'brian',
+    extends: 'eslint-config-limeandcoconut',
+    // parser: 'vue-eslint-parser',
+    plugins: [
+        'html',
+        // TODO: [2019-10-1] Check that eslint-plugin-vue has been upgraded to work with eslint ^6
+        // 'vue',
+        'unicorn',
+    ],
+    rules: {
+    },
 }
